dd407a8528a39d1cd9a3dc83df8c3f9142759237
farisnif <fan5061@psu.edu>
2023-02-18 15:15:54 -0500

    most JS functioning except detail btn paragraph

--- src/flight-card.js
+++ src/flight-card.js
@@ -38,17 +38,35 @@ h2{
   display: none;
   clear: both;
 }
+.details{
+  display:block;
+  margin: 0 auto;
+  text-align: center;
+  background-color: navajowhite;
+}
 p{
   text-align: center;
   display: none;
 }
 @media (min-width: 500px) and (max-width: 800px) {
-  .buttons {
+  .details {
     display: block;
     margin: 0 auto;
     text-align: center;
   }
 }
+  .details:hover{
+    background: linear-gradient(
+    to right,
+    red,
+    yellow,
+    lightseagreen,
+    white,
+    lightseagreen,
+    yellow,
+    red
+  );
+  }
 .somethingelse {
   font-size: 18px;
   display: block;
@@ -138,26 +156,27 @@ p{
   constructor() {
     super();
     this.version = 'STARTING';
+
   }
 
   render() {
     return html`
     <div class="crd">
-  <div class="container">
+    <div class="container">
     <div class="header">
       <h1>A Picture of a Sunset</h1>
-      <img class="image" src="https://media.cntraveler.com/photos/576834ff90b3537d7c010c05/16:9/w_2560%2Cc_limit/GettyImages-162672165.jpg" alt="A picture"/>
+    </div>
+    <img class="image" src="https://media.cntraveler.com/photos/576834ff90b3537d7c010c05/16:9/w_2560%2Cc_limit/GettyImages-162672165.jpg" alt="A picture"/>
       <div class="subheader">
         <h2>A Picture of a Plane in a Sunset</h2>
       </div>
-    </div>
   </div>
   <div class="paragraph">
     <p>This is a picture of a Boeing 787 taking off during sunset.</p>
   </div>
-  <div class="buttons">
-    <button class="details">Details</button>
-  </div>
+
+  <button class="details">Details</button>
+
   <meme-maker image-url="https://media.cntraveler.com/photos/576834ff90b3537d7c010c05/16:9/w_2560%2Cc_limit/GettyImages-162672165.jpg"
       top-text="You can't fail your tests"
       bottom-text="If you don't write any"
